eb429685819b7843fa0d621dc4f97d02bb566171
ninerats <ninerats@gmail.com>
2017-01-29 19:40:38 -0800

    add Remoting project

---
+++ Remoting/InvokeRemote1.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+param()
+#
+# Script.ps1
+#
+Write-Verbose "Local verbose"
+Write-Information "Local Information"
+Write-Debug "Local Debug"
+Write-Output "Calling remote1..."
+
+$passthruverb = @'
+	$VerbosePreference= $Using:VerbosePreference
+	$DebugPreference = $Using:DebugPreference
+'@
+$cmd = {
+	$VerbosePreference= $Using:VerbosePreference
+	$DebugPreference = $Using:DebugPreference
+	& "D:\git\Lab\ps1\RemoteJobs\remote1.ps1"
+}
+Invoke-Command -ComputerName dolly -Script $cmd
+Write-Output "Complete."
